22d3e93724f1227edd059a232c57db76653a909e
AIexMa <alexandra247ma@gmail.com>
2025-11-24 10:09:40 -0800

    Add files via upload

---
+++ website.js
@@ -0,0 +1,25 @@
+function myFunction() {
+
+document.getElementById("nood").innerHTML = "1/2 pound of noodles";
+document.getElementById("oliveoil").innerHTML = "1 tablespoons pure olive oil";
+document.getElementById("dragoneggs").innerHTML = "2 eggs";
+document.getElementById("bakon").innerHTML = "3 thick slices of smoky bacon";
+document.getElementById("cheezeburger").innerHTML = "Small chunk of Parmesean cheese, freshly grated";
+
+}
+function servings2() {
+
+document.getElementById("nood").innerHTML = "1 pound of noodles";
+document.getElementById("oliveoil").innerHTML = "2 tablespoons pure olive oil";
+document.getElementById("dragoneggs").innerHTML = "4 eggs";
+document.getElementById("bakon").innerHTML = "6 thick slices of smoky bacon";
+document.getElementById("cheezeburger").innerHTML = "Chunk of Parmesean cheese, freshly grated";
+
+}
+function stuff() {
+    document.getElementById("review").innerHTML = "Thank you for your feedback."
+}
+function yaa() {
+    let x = document.getElementById("mySelect").value;
+  document.getElementById("demo").innerHTML = x;
+}
